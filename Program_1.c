f8ecccdcfda718e17a27561d0cbefcfd26c13a9c
Elaine14 <>
2021-05-29 11:31:45 +0000

    print the vowels in a word

--- Program_1.c
+++ Program_1.c
@@ -5,7 +5,23 @@
 int main()
 {
 	char str[10];
-	scanf("%s", str);
+  int i,n;
+
+  printf("Enter a word:");
+	scanf("%s",str); 
+
+  while(str[i]!='\0')
+  {
+    if(str[i]=='A'||str[i]=='a'||str[i]=='E'||str[i]=='e'||str[i]=='I'||str[i]=='i'||str[i]=='O'||str[i]=='o'||str[i]=='U'||str[i]=='u')
+    {
+      n++;
+      i++;
+    }
+  }
+ printf("The number of vowels in the string are:%d",n);
+  
+
+  
 
 	//Write your code here
 
