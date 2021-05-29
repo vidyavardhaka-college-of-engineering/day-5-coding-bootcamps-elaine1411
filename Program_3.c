e9c4a845d93851d4752a4e93e6bebc852aa21cd6
Elaine14 <>
2021-05-29 11:44:42 +0000

    to check weather a given string is a palindrome

--- Program_3.c
+++ Program_3.c
@@ -6,12 +6,26 @@
 //output:
 //Palindrome
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
 	char str[10];
+  int i,length,flag=0;
+
+  printf("Enter a string:");
 	scanf("%s", str);
 
+  length=strlen(str);
+
+  for(i=0;i<length;i++)
+  {
+    if(str[i]!=str[length-i-1])
+    {
+      flag=1;
+    }
+    break;
+  }
 	//Write your code here
 	
 	return 0;
